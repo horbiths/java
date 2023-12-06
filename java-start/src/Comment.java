5f5f6c3635acf18ce157636328d24e3c3718cd8f
horbiths <gkrtjs77@gmail.com>
2023-12-07 00:08:50 +0900

    Comment.java class 추가

---
+++ java-start/src/Comment.java
@@ -0,0 +1,13 @@
+public class Comment { //Comment 클래스의 범위 시작
+	public static void main(String[] args) { // main() 메서드의 범위 시작
+		System.out.println("Hello Java1"); // Hello Java1을 출력한다. (한 줄 주석 부분 적용)
+		//System.out.println("Hello Java2"); 한 줄 주석 전체 적용
+		//한 줄 주석 이 기호 이후의 모든 텍스트는 주석으로 처리된다.
+		/* 여러 줄 주석
+		System.out.println("Hello Java3");
+		System.out.println("Hello Java4");
+		이 사이의 모든 텍스트는 주석으로 처리된다.
+		*/
+
+	}
+}
