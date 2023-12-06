0243d0ab5101cbffcdf0fe929e73b1b84d1852ed
horbiths <gkrtjs77@gmail.com>
2023-12-06 23:33:40 +0900

    start

---
+++ java-start/src/HelloJava.java
@@ -0,0 +1,13 @@
+//HelloJava라는 클래스를 만들었다.
+//파일명과 클래스의 이름은 같아야 한다.
+public class HelloJava { //HelloJava 클래스의 범위 시작
+
+  //main(String[] args)라는 메서드를 찾아서 프로그램을 시작한다.
+  //{} 블록을 사용해서 클래스의 시작과 끝을 나타낸다.
+  public static void main(String[] args) { //main() 메서드의 범위 시작
+    // System.out.println()은 값을 콘솔에 출력한다.
+    //자바는 문자열을 사용할 때 ""(쌍따옴표)를 사용한다.
+    //자바는 ;(세미콜론)으로 문장을 구분한다. 문장이 끝나면 세미콜론을 필수로 넣어줘야 한다.
+    System.out.println("Hello Java");
+  }//main() 메서드의 범위 끝
+} //HelloJava 클래스의 범위 끝
